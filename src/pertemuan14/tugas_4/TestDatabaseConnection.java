725c6a26d281fddd09ef95e677abe81a881dc4b4
Kresna Satria <145238961+kresnasatria@users.noreply.github.com>
2025-06-03 15:20:14 +0700

    Mengerjakan Tugas 4

---
+++ src/pertemuan14/tugas_4/TestDatabaseConnection.java
@@ -0,0 +1,10 @@
+package Tugas_4;
+
+public class TestDatabaseConnection {
+    public static void main(String[] args) {
+        // Saat pertama kali mengakses DatabaseConnection.connectionString,
+        // static block di DatabaseConnection sudah otomatis dijalankan.
+        System.out.println("Nilai connectionString: " + DatabaseConnection.connectionString);
+    }
+}
+
